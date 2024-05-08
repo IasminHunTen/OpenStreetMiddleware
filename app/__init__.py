f094c738e3e2af87b97872f3ffa47feea979623c
Admin <constantiniasmin321@yahoo.com>
2024-05-08 16:38:30 +0300

    i think it will work now

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 import json
 import os
+from pathlib import Path
 
 from flask import Flask
 
@@ -7,10 +8,15 @@ from extentions import api, cors, cache, open_street_client
 from routes.main_api import ns
 
 
+def load_config():
+    config_file = os.path.join(Path(__file__).parent.parent.absolute(), "config", "default_config.json")
+    with open(config_file) as fd:
+        return json.load(fd)
+
+
 def create_app():
     app = Flask(__name__)
-    with open("../config/default_config.json") as fd:
-        app.config.update(json.load(fd))
+    app.config.update(load_config())
     api.init_app(app)
     api.add_namespace(ns)
     cors.init_app(app)
